10940c639b91996c37a0a0b11a01a8b09dc0fca1
Phyllip Hamby <phyllip.hamby@rfsmart.com>
2020-09-09 08:38:43 -0400

    a conflicting commit

--- node/routes/index.js
+++ node/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Goodbye Git!' });
 });
 
 module.exports = router;

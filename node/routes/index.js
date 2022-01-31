70354dc912fbfe04724a5e6f1433ea0939d3aae1
lovasij <lovasi.janos@gmail.com>
2022-01-31 12:43:09 +0100

    version 2

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Git!' });
+  res.render('index', { title: 'Hello Git2!' });
 });
 
 module.exports = router;

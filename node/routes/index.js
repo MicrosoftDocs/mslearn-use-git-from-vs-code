c88f71565df85fd5477e0afc9a8f199f7a962bc8
rubisco57 <rubisco57@gmail.com>
2022-07-12 13:48:26 +0200

    2nd comit and push

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- node/routes/users.js
+++ node/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing. Bat */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });

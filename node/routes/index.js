b2ee8add6c7cbe51bae4a330b026190be8f9f1f6
Matthew Daniels <matthew.b.daniels52@outlook.com>
2022-07-13 17:52:46 -0400

    update

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Git!' });
+  res.render('index', { title: 'Howdy!' });
 });
 
 module.exports = router;

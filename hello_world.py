3d0f65a41330187394830050e6d74bc3001fe21e
carloscruz01 <contato.carloscruz29@gmail.com>
2022-11-28 15:33:41 -0300

    Uso da função.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,17 @@
+lt = []
 declaracao = True
 while declaracao:
     msg = str(input('Insira sua mensagem:'))
     if msg != '':
         print(msg)
-        declaracao = False
+        print("test")        
+        print(0, 1, 1, 0, 0)
+        dsc = {
+            'mensagem': msg
+        }
+        lt.append(dsc)
     else:
         print('finish process.')
-    
+        declaracao = False
+for dsc in lt:
+    print('Mensagem:', dsc['mensagem'])   

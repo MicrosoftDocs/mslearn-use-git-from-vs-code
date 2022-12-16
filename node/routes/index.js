042ac1bbc0612234a88f9373adeccf9297e2d309
zkpunc <angelosabokkb62@gmail.com>
2022-12-16 18:07:23 +0800

    mergetest

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello git' });
+  res.render('index', { title: 'Hello git merge test' });
 });
 
 module.exports = router;
